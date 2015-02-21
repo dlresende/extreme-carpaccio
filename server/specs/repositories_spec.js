3c382d858eff74ff16034dfb841accd49f07ab11
Diego Lemos <diegolemosderesende@gmail.com>
2015-02-21 23:53:14 +0100

    Fix precision intervals.

--- server/specs/repositories_spec.js
+++ server/specs/repositories_spec.js
@@ -102,15 +102,31 @@ describe('Reductions', function() {
         reductions = new Reductions();
     });
 
-    it('should be reduced by 5% when total is less than or equal to 5 000', function() {
+    it('should de reduced by 15% when total is bigger than 50,000', function() {
+        expect(reductions.reductionFor(50001)).toBe(0.15);
+    });
+
+    it('should de reduced by 10% when total is between [10,000, 50,000)', function() {
+        expect(reductions.reductionFor(10000)).toBe(0.10);
+        expect(reductions.reductionFor(10500)).toBe(0.10);
+    });
+
+    it('should de reduced by 7% when total is between [7,000, 10,000)', function() {
+        expect(reductions.reductionFor(7000)).toBe(0.07);
+        expect(reductions.reductionFor(7500)).toBe(0.07);
+    });
+
+    it('should be reduced by 5% when total is between [5,000, 7,000)', function() {
+        expect(reductions.reductionFor(5000)).toBe(0.05);
         expect(reductions.reductionFor(5500)).toBe(0.05);
     });
 
-    it('should not be reduced when when the total is less than or equal to 1 000', function() {
-        expect(reductions.reductionFor(500)).toBe(0.00);
+    it('should be reduced by 3% when total is between [1,000, 5,000)', function() {
+        expect(reductions.reductionFor(1000)).toBe(0.03);
+        expect(reductions.reductionFor(1100)).toBe(0.03);
     });
 
-    it('should de reduced by 15% when total is less than or equal to 50 000', function() {
-        expect(reductions.reductionFor(55000)).toBe(0.15);
+    it('should not be reduced when when the total between [0, 1,000)', function() {
+        expect(reductions.reductionFor(500)).toBe(0.00);
     });
 });
