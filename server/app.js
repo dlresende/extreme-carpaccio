776c1714877cbfe6b246a9c976a7c10f2065bc4b
dotnetstation <rad_hass@hotmail.com>
2015-02-24 18:37:58 +0100

    version with reactjs

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.static('views'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
