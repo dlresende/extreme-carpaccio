d2547f2a0699cf3f8d162be48af9a5732c1415fd
Arnauld <arnauld.loyer@gmail.com>
2015-02-25 20:26:32 +0100

    polishing

--- clients/erlang-alt/src/json_utils.erl
+++ clients/erlang-alt/src/json_utils.erl
@@ -1,15 +1,30 @@
 -module(json_utils).
 
-
+%% API
 -export([decode/1,
          encode/1,
          get_value/2]).
 
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+%%
+%% @doc decode/1 decode a json text into Erlang datastructure
+%%
 decode(Source) ->
   jsx:decode(Source).
 
+%%
+%% @doc encode/1 encode an Erlang datastructure into a json text
+%%
 encode(Data) ->
   jsx:encode(Data).
+
+%%
+%% @doc get_value/2 lookup a named tuple within the datastructure and
+%%    either return the associated value `{ok, ValueFound}` or `not_found`
+%%
 
 get_value([], _Searched) ->
     not_found;
