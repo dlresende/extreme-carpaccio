8f9ed6c88f35d4180f038e025bb0b4d5c085c5e0
Diego Lemos <diegolemosderesende@gmail.com>
2015-02-28 20:04:00 +0100

    Cleaning unused js dependencies.

--- server/app.js
+++ server/app.js
@@ -1,33 +1,23 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var services = require('./javascripts/services');
 var repositories = require('./javascripts/repositories');
+var routes = require('./javascripts/routes');
 
 var sellers = new repositories.Sellers();
-
 var sellerService = new services.SellerService(sellers);
 var orderService = new services.OrderService();
 var dispatcher = new services.Dispatcher(sellerService, orderService);
 
-var routes = require('./javascripts/routes')(sellerService);
-
 var app = express();
 
-app.set('views', __dirname);
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', routes(sellerService));
 
 dispatcher.startBuying(5000);
 
--- server/javascripts/routes.js
+++ server/javascripts/routes.js
@@ -3,20 +3,22 @@ var express = require('express'),
 
 module.exports = function (sellerService) {
     var router = express.Router();
+    var OK = 200;
+    var BAD_REQUEST = 400;
 
     router.get('/sellers', function(request, response) {
-        response.send('sellers', sellerService.allSellers() );
+        response.status(OK).send(sellerService.allSellers());
     });
 
     router.post('/seller', function(request, response) {
         var sellerName = request.body.name,
             sellerUrl = request.body.url;
+
         if(_.isEmpty(sellerName) || _.isEmpty(sellerUrl)) {
-          response.status(400).send({"message":"missing name or url"});
-        }
-        else {
+          response.status(BAD_REQUEST).send({message:'missing name or url'});
+        } else {
           sellerService.register(sellerUrl, sellerName);
-          response.status(200).end();
+          response.status(OK).end();
         }
     });
 
