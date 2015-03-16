e4299547ed9935746f81ddb9bb6dc3c1a77226f6
Diego Lemos <diegolemosderesende@gmail.com>
2015-03-16 22:51:42 +0100

    Moving reduction tests to service_specs and delete dead code.

--- server/specs/repositories_spec.js
+++ server/specs/repositories_spec.js
@@ -126,38 +126,3 @@ describe('Countries', function() {
     });
 });
 
-describe('Reductions', function() {
-    var reductions;
-
-    beforeEach(function() {
-        reductions = new Reductions();
-    });
-
-    it('should de reduced by 15% when total is bigger than 50,000', function() {
-        expect(reductions.reductionFor(50001)).toBe(0.15);
-    });
-
-    it('should de reduced by 10% when total is between [10,000, 50,000)', function() {
-        expect(reductions.reductionFor(10000)).toBe(0.10);
-        expect(reductions.reductionFor(10500)).toBe(0.10);
-    });
-
-    it('should de reduced by 7% when total is between [7,000, 10,000)', function() {
-        expect(reductions.reductionFor(7000)).toBe(0.07);
-        expect(reductions.reductionFor(7500)).toBe(0.07);
-    });
-
-    it('should be reduced by 5% when total is between [5,000, 7,000)', function() {
-        expect(reductions.reductionFor(5000)).toBe(0.05);
-        expect(reductions.reductionFor(5500)).toBe(0.05);
-    });
-
-    it('should be reduced by 3% when total is between [1,000, 5,000)', function() {
-        expect(reductions.reductionFor(1000)).toBe(0.03);
-        expect(reductions.reductionFor(1100)).toBe(0.03);
-    });
-
-    it('should not be reduced when when the total between [0, 1,000)', function() {
-        expect(reductions.reductionFor(500)).toBe(0.00);
-    });
-});
--- server/specs/services_spec.js
+++ server/specs/services_spec.js
@@ -192,3 +192,39 @@ describe('Dispatcher', function() {
         expect(orderService.sendOrder).toHaveBeenCalledWith(bob, order, jasmine.any(Function), jasmine.any(Function));
     });
 });
+
+describe('Standard Reduction', function() {
+    var standardReduction;
+
+    beforeEach(function() {
+        standardReduction = Reduction.STANDARD;
+    });
+
+    it('should de reduced by 15% when total is bigger than 50,000', function() {
+        expect(standardReduction.reductionFor(50001)).toBe(0.15);
+    });
+
+    it('should de reduced by 10% when total is between [10,000, 50,000)', function() {
+        expect(standardReduction.reductionFor(10000)).toBe(0.10);
+        expect(standardReduction.reductionFor(10500)).toBe(0.10);
+    });
+
+    it('should de reduced by 7% when total is between [7,000, 10,000)', function() {
+        expect(standardReduction.reductionFor(7000)).toBe(0.07);
+        expect(standardReduction.reductionFor(7500)).toBe(0.07);
+    });
+
+    it('should be reduced by 5% when total is between [5,000, 7,000)', function() {
+        expect(standardReduction.reductionFor(5000)).toBe(0.05);
+        expect(standardReduction.reductionFor(5500)).toBe(0.05);
+    });
+
+    it('should be reduced by 3% when total is between [1,000, 5,000)', function() {
+        expect(standardReduction.reductionFor(1000)).toBe(0.03);
+        expect(standardReduction.reductionFor(1100)).toBe(0.03);
+    });
+
+    it('should not be reduced when when the total between [0, 1,000)', function() {
+        expect(standardReduction.reductionFor(500)).toBe(0.00);
+    });
+});
