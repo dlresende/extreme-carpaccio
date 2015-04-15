b62f0ac2622005b842914f321290d6ad24b71529
Diego Lemos <diegolemosderesende@gmail.com>
2015-04-15 19:18:18 +0200

    [clients][java-httpserver] Introduce Response object in order to simplify code design.

--- clients/java/java-httpserver/src/main/java/xcarpaccio/MyHttpServer.java
+++ clients/java/java-httpserver/src/main/java/xcarpaccio/MyHttpServer.java
@@ -9,26 +9,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.Arrays;
 
+import static xcarpaccio.MyHttpServer.Response.ok;
 import static xcarpaccio.StringUtils.stringify;
 
 public class MyHttpServer
 {
     private final int port;
     private final Logger logger;
+
     private HttpServer server;
 
-    MyHttpServer(int port) {
-        this.port = port;
-        this.logger = new Logger();
-    }
-
-    MyHttpServer(int port, Logger logger) {
+    public MyHttpServer(int port, Logger logger) {
         this.port = port;
         this.logger = logger;
     }
 
-    void start() throws IOException {
+    public void start() throws IOException {
         server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext("/ping", new PingHttpHandler());
         server.createContext("/feedback", new FeedbackHttpHandler());
@@ -38,7 +36,7 @@ public class MyHttpServer
         logger.log("Server running on port " + port + "...");
     }
 
-    void shutdown() {
+    public void shutdown() {
         if(server != null) {
             logger.log("Stopping server...");
             server.stop(2);
@@ -46,28 +44,31 @@ public class MyHttpServer
     }
 
     public static void main( String[] args ) throws IOException {
-        new MyHttpServer(9000).start();
+        Logger logger = new Logger();
+        new MyHttpServer(9000, logger).start();
     }
 
     private abstract class AbstractHttpHandler implements HttpHandler {
-        protected static final String NO_CONTENT = "";
-
         @Override
         public void handle(HttpExchange httpExchange) throws IOException {
-            String response = respond(httpExchange);
-            httpExchange.sendResponseHeaders(200, response.length());
-            OutputStream os = httpExchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            Response response = doHandle(httpExchange);
+            respond(httpExchange, response);
         }
 
-        public abstract String respond(HttpExchange httpExchange);
+        public abstract Response doHandle(HttpExchange request);
+
+        private void respond(HttpExchange httpExchange, Response response) throws IOException {
+            httpExchange.sendResponseHeaders(response.getStatusCode(), response.getBody().length);
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBody());
+            os.close();
+        }
     }
 
     private class PingHttpHandler extends AbstractHttpHandler {
         @Override
-        public String respond(HttpExchange httpExchange) {
-            return "pong";
+        public Response doHandle(HttpExchange request) {
+            return ok("pong");
         }
     }
 
@@ -75,8 +76,8 @@ public class MyHttpServer
         protected final ObjectMapper objectMapper = new ObjectMapper();
 
         @Override
-        public String respond(HttpExchange httpExchange) {
-            InputStream body = httpExchange.getRequestBody();
+        public Response doHandle(HttpExchange request) {
+            InputStream body = request.getRequestBody();
 
             try {
                 Message message = objectMapper.readValue(body, Message.class);
@@ -85,17 +86,66 @@ public class MyHttpServer
                 logger.error(exception.getMessage());
             }
 
-            return NO_CONTENT;
+            return ok();
         }
     }
 
     private class ConsoleHttpHandler extends AbstractHttpHandler {
         @Override
-        public String respond(HttpExchange httpExchange) {
-            String method = httpExchange.getRequestMethod();
-            String uri = httpExchange.getRequestURI().getPath();
-            logger.log(method + " " + uri + " " + stringify(httpExchange.getRequestBody()));
-            return NO_CONTENT;
+        public Response doHandle(HttpExchange request) {
+            String method = request.getRequestMethod();
+            String uri = request.getRequestURI().getPath();
+            logger.log(method + " " + uri + " " + stringify(request.getRequestBody()));
+            return ok();
+        }
+    }
+
+    public static class Response {
+        private static final byte[] NO_CONTENT = new byte[]{};
+
+        private final int statusCode;
+        private final byte[] body;
+
+        private Response(int statusCode, byte[] body) {
+            this.statusCode = statusCode;
+            this.body = body;
+        }
+
+        public static Response ok() {
+            return ok(NO_CONTENT);
+        }
+
+        public static Response ok(byte[] body) {
+            return new Response(200, body);
+        }
+
+        public static Response ok(String body) {
+            return ok(body != null ? body.getBytes() : NO_CONTENT);
+        }
+
+        public int getStatusCode() {
+            return statusCode;
+        }
+
+        public byte[] getBody() {
+            return body;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Response response = (Response) o;
+
+            return statusCode == response.statusCode && Arrays.equals(body, response.body);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = statusCode;
+            result = 31 * result + (body != null ? Arrays.hashCode(body) : 0);
+            return result;
         }
     }
 }
