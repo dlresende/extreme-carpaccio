51de19ab9535afc11db35cd21964f144e80d920a
Alexandre Victoor <alexandre.victoor@fluo.com>
2019-02-07 14:03:15 +0100

    Add a Typescript client

---
+++ clients/typescript/src/server.ts
@@ -0,0 +1,25 @@
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import bodyParser = require("koa-bodyparser");
+
+const app = new Koa();
+app.use(bodyParser());
+const router = new Router();
+
+router.get('/*', async (ctx) => {
+    ctx.body = 'Hello World!';
+});
+
+router.post('/feedback', async (ctx) => {
+    console.log('Feedback received', ctx.request.body);
+});
+
+router.post('/order', async (ctx) => {
+    // your code here
+});
+
+app.use(router.routes());
+
+app.listen(8080);
+
+console.log('Server running on port 8080');
