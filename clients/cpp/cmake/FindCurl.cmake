ee227b3cbdd7bbbcba953fb0ab5c0d9a91c4fb8e
Damien Menanteau <damien.menanteau@gmail.com>
2021-05-27 17:14:40 +0200

    Remove FindCurl.cmake and FindHttpClient.cmake

--- clients/cpp/cmake/FindCurl.cmake
+++
@@ -1,7 +0,0 @@
-# Fetch googletest
-
-CPMFindPackage(
-  NAME HTTPClient
-  GITHUB_REPOSITORY embeddedmz/httpclient-cpp
-  GIT_TAG master
-)
--- clients/cpp/cmake/FindHttpClient.cmake
+++
@@ -1,8 +0,0 @@
-# Fetch googletest
-
-CPMFindPackage(
-  NAME googletest
-  GITHUB_REPOSITORY google/googletest
-  GIT_TAG master
-  OPTIONS "INSTALL_GTEST OFF" "gtest_force_shared_crt" "BUILD_GMOCK OFF"
-)
