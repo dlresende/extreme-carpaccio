debc3652b784e23b53f0040a0757a55b0dccf398
Damien Menanteau <damien.menanteau@gmail.com>
2021-05-28 12:47:40 +0200

    Upgrade nlhmann_json to latest version (3.9.1)

--- clients/cpp/cmake/FindNlohmannJson.cmake
+++ clients/cpp/cmake/FindNlohmannJson.cmake
@@ -2,22 +2,14 @@
 
 CPMFindPackage(
   NAME nlohmann_json
-  VERSION 3.6.1
+  VERSION 3.9.1
   # the git repo is incredibly large, so we download the archived include directory
-  URL https://github.com/nlohmann/json/releases/download/v3.6.1/include.zip
-  URL_HASH SHA256=69cc88207ce91347ea530b227ff0776db82dcb8de6704e1a3d74f4841bc651cf
+  URL https://github.com/nlohmann/json/releases/download/v3.9.1/include.zip
+  URL_HASH SHA256=6bea5877b1541d353bd77bdfbdb2696333ae5ed8f9e8cc22df657192218cad91
+  OPTIONS "JSON_BuildTests OFF"
 )
 
 if (nlohmann_json_ADDED)
   add_library(nlohmann_json INTERFACE IMPORTED)
   target_include_directories(nlohmann_json INTERFACE ${nlohmann_json_SOURCE_DIR})
 endif()
-
-# This is the latest package provided on CPM repo, but does not seem to work
-
-#CPMAddPackage(
-#  NAME nlohmann_json
-#  VERSION 3.9.1
-#  OPTIONS
-#    "JSON_BuildTests OFF"
-#)
