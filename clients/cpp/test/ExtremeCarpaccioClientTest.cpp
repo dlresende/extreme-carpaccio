6683990ef1926de0078c40fb3f372819fd0e948d
Damien Menanteau <damien.menanteau@gmail.com>
2021-06-03 15:44:30 +0200

    Add raw HTTP call

--- clients/cpp/test/ExtremeCarpaccioClientTest.cpp
+++ clients/cpp/test/ExtremeCarpaccioClientTest.cpp
@@ -1,16 +1,78 @@
 
 #include <extreme_carpaccio_client/ExtremeCarpaccioClient.hpp>
 
-#include <boost/beast/core.hpp>
-
 #include <gtest/gtest.h>
 
 #include <vector>
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+namespace beast = boost::beast;     // from <boost/beast.hpp>
+namespace http = beast::http;       // from <boost/beast/http.hpp>
+namespace net = boost::asio;        // from <boost/asio.hpp>
+using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+
 
 using namespace extreme_carpaccio_client;
 
 TEST(ExtremeCarpaccioClient, should_handle_feedback)
 {
+
+   std::string host = "localhost";
+   std::string port = "8081";
+   std::string target = "/feedback";
+   int version = 11;
+
+   net::io_context ioc;
+
+   // These objects perform our I/O
+   tcp::resolver resolver(ioc);
+   beast::tcp_stream stream(ioc);
+
+   // Look up the domain name
+   auto const results = resolver.resolve(host, port);
+
+   // Make the connection on the IP address we get from a lookup
+   stream.connect(results);
+
+   // Set up an HTTP GET request message
+   http::request<http::string_body> req{ http::verb::post, target, version };
+   req.set(http::field::host, host);
+   req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+   // Send the HTTP request to the remote host
+   http::write(stream, req);
+
+   // This buffer is used for reading and must be persisted
+   beast::flat_buffer buffer;
+
+   // Declare a container to hold the response
+   http::response<http::dynamic_body> res;
+
+   // Receive the HTTP response
+   http::read(stream, buffer, res);
+
+   // Write the message to standard out
+   std::cout << res << std::endl;
+
+   // Gracefully close the socket
+   beast::error_code ec;
+   stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+
+   // not_connected happens sometimes
+   // so don't bother reporting it.
+   //
+   if (ec && ec != beast::errc::not_connected)
+      throw beast::system_error{ ec };
+
+
    EXPECT_EQ(1, 1);
 }
 
