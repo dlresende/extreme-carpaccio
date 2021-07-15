e8b96b55ec624c44c4379278ee745aef0a4b0ac1
NADIF Antoine <Antoine.NADIF@murex.com>
2021-07-15 11:44:11 +0200

    Integrate Boost Beast fast server example as is to have a starting point.

--- clients/cpp/src/extreme_carpaccio_client/ExtremeCarpaccioClient.cpp
+++ clients/cpp/src/extreme_carpaccio_client/ExtremeCarpaccioClient.cpp
@@ -5,7 +5,6 @@
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
 #include <boost/asio.hpp>
-#include <boost/filesystem.hpp>
 #include <chrono>
 #include <cstdlib>
 #include <cstring>
@@ -14,97 +13,273 @@
 #include <memory>
 #include <string>
 
-#include <cstdlib>
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
-namespace ip = boost::asio::ip;         // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio.hpp>
-namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
+// Return a reasonable mime type based on the extension of a file.
+beast::string_view
+mime_type(beast::string_view path)
+{
+   using beast::iequals;
+   auto const ext = [&path]
+   {
+      auto const pos = path.rfind(".");
+      if (pos == beast::string_view::npos)
+         return beast::string_view{};
+      return path.substr(pos);
+   }();
+   if (iequals(ext, ".htm"))  return "text/html";
+   if (iequals(ext, ".html")) return "text/html";
+   if (iequals(ext, ".php"))  return "text/html";
+   if (iequals(ext, ".css"))  return "text/css";
+   if (iequals(ext, ".txt"))  return "text/plain";
+   if (iequals(ext, ".js"))   return "application/javascript";
+   if (iequals(ext, ".json")) return "application/json";
+   if (iequals(ext, ".xml"))  return "application/xml";
+   if (iequals(ext, ".swf"))  return "application/x-shockwave-flash";
+   if (iequals(ext, ".flv"))  return "video/x-flv";
+   if (iequals(ext, ".png"))  return "image/png";
+   if (iequals(ext, ".jpe"))  return "image/jpeg";
+   if (iequals(ext, ".jpeg")) return "image/jpeg";
+   if (iequals(ext, ".jpg"))  return "image/jpeg";
+   if (iequals(ext, ".gif"))  return "image/gif";
+   if (iequals(ext, ".bmp"))  return "image/bmp";
+   if (iequals(ext, ".ico"))  return "image/vnd.microsoft.icon";
+   if (iequals(ext, ".tiff")) return "image/tiff";
+   if (iequals(ext, ".tif"))  return "image/tiff";
+   if (iequals(ext, ".svg"))  return "image/svg+xml";
+   if (iequals(ext, ".svgz")) return "image/svg+xml";
+   return "application/text";
+}
 
 class http_worker
 {
 public:
-http_worker(tcp::acceptor& acceptor, const std::string& doc_root)
-   : m_acceptor(acceptor)
-   , m_doc_root(doc_root)
-   , m_socket(m_acceptor.get_executor())
-{
-}
+   http_worker(http_worker const&) = delete;
+   http_worker& operator=(http_worker const&) = delete;
 
-void send_bad_response(
-        http::status status,
-        std::string const& error)
-{
-   http::response<http::string_body> string_response_;
+   http_worker(tcp::acceptor& acceptor, const std::string& doc_root) :
+      acceptor_(acceptor),
+      doc_root_(doc_root)
+   {
+   }
 
-   string_response_.result(status);
-   string_response_.keep_alive(false);
-   string_response_.set(http::field::server, "Beast");
-   string_response_.set(http::field::content_type, "text/plain");
-   string_response_.body() = error;
-   string_response_.prepare_payload();
-
-   http::response_serializer<http::string_body> string_serializer(string_response_);
-
-   http::async_write(
-      m_socket,
-      string_serializer,
-      [this](boost::beast::error_code ec, std::size_t)
-      {
-         m_socket.shutdown(tcp::socket::shutdown_send, ec);
-         accept();
-      });
-}
-
-
-void read_request()
-{
-   // On each read the parser needs to be destroyed and
-   // recreated. We store it in a boost::optional to
-   // achieve that.
-   //
-   // Arguments passed to the parser constructor are
-   // forwarded to the message object. A single argument
-   // is forwarded to the body constructor.
-   //
-   // We construct the dynamic body with a 1MB limit
-   // to prevent vulnerability to buffer attacks.
-   //
-
-   //http::request_parser<http::string_body> parser(std::piecewise_construct, std::make_tuple());
-
-   //http::async_read(
-   //   m_socket,
-   //   m_buffer,
-   //   parser,
-   //   [this](boost::beast::error_code ec, std::size_t)
-   //   {
-         send_bad_response(http::status::bad_request, "Invalid request-method");
-      //});
-}
-
-void accept()
-{
-   m_acceptor.async_accept(
-      m_socket,
-      [this](boost::beast::error_code ec)
-      {
-         read_request();
-      });
-
-}
-
-void start()
-{
-   accept();
-}
+   void start()
+   {
+      accept();
+      check_deadline();
+   }
 
 private:
-   tcp::acceptor& m_acceptor;
-   const std::string& m_doc_root;
-   tcp::socket m_socket;
-   boost::beast::flat_static_buffer<8192> m_buffer;
-};
+   using alloc_t = std::allocator<char>;
+   //using request_body_t = http::basic_dynamic_body<beast::flat_static_buffer<1024 * 1024>>;
+   using request_body_t = http::string_body;
 
+   // The acceptor used to listen for incoming connections.
+   tcp::acceptor& acceptor_;
+
+   // The path to the root of the document directory.
+   std::string doc_root_;
+
+   // The socket for the currently connected client.
+   tcp::socket socket_{ acceptor_.get_executor() };
+
+   // The buffer for performing reads
+   beast::flat_static_buffer<8192> buffer_;
+
+   // The allocator used for the fields in the request and reply.
+   alloc_t alloc_;
+
+   // The parser for reading the requests
+   boost::optional<http::request_parser<request_body_t, alloc_t>> parser_;
+
+   // The timer putting a time limit on requests.
+   net::steady_timer request_deadline_{
+       acceptor_.get_executor(), (std::chrono::steady_clock::time_point::max)() };
+
+   // The string-based response message.
+   boost::optional<http::response<http::string_body, http::basic_fields<alloc_t>>> string_response_;
+
+   // The file-based response message.
+   boost::optional<http::response<http::file_body, http::basic_fields<alloc_t>>> file_response_;
+
+   void accept()
+   {
+      // Clean up any previous connection.
+      beast::error_code ec;
+      socket_.close(ec);
+      buffer_.consume(buffer_.size());
+
+      acceptor_.async_accept(
+         socket_,
+         [this](beast::error_code ec)
+      {
+         if (ec)
+         {
+            accept();
+         }
+         else
+         {
+            // Request must be fully processed within 60 seconds.
+            request_deadline_.expires_after(
+               std::chrono::seconds(60));
+
+            read_request();
+         }
+      });
+   }
+
+   void read_request()
+   {
+      // On each read the parser needs to be destroyed and
+      // recreated. We store it in a boost::optional to
+      // achieve that.
+      //
+      // Arguments passed to the parser constructor are
+      // forwarded to the message object. A single argument
+      // is forwarded to the body constructor.
+      //
+      // We construct the dynamic body with a 1MB limit
+      // to prevent vulnerability to buffer attacks.
+      //
+      parser_.emplace(
+         std::piecewise_construct,
+         std::make_tuple(),
+         std::make_tuple(alloc_));
+
+      http::async_read(
+         socket_,
+         buffer_,
+         *parser_,
+         [this](beast::error_code ec, std::size_t)
+      {
+         if (ec)
+            accept();
+         else
+            process_request(parser_->get());
+      });
+   }
+
+   void process_request(http::request<request_body_t, http::basic_fields<alloc_t>> const& req)
+   {
+      switch (req.method())
+      {
+      case http::verb::get:
+         send_file(req.target());
+         break;
+
+      default:
+         // We return responses indicating an error if
+         // we do not recognize the request method.
+         send_bad_response(
+            http::status::bad_request,
+            "Invalid request-method '" + std::string(req.method_string()) + "'\r\n");
+         break;
+      }
+   }
+
+   void send_bad_response(
+      http::status status,
+      std::string const& error)
+   {
+      string_response_.emplace(
+         std::piecewise_construct,
+         std::make_tuple(),
+         std::make_tuple(alloc_));
+
+      string_response_->result(status);
+      string_response_->keep_alive(false);
+      string_response_->set(http::field::server, "Beast");
+      string_response_->set(http::field::content_type, "text/plain");
+      string_response_->body() = error;
+      string_response_->prepare_payload();
+
+      http::async_write(
+         socket_,
+         *string_response_,
+         [this](beast::error_code ec, std::size_t)
+      {
+         socket_.shutdown(tcp::socket::shutdown_send, ec);
+         string_response_.reset();
+         accept();
+      });
+   }
+
+   void send_file(beast::string_view target)
+   {
+      // Request path must be absolute and not contain "..".
+      if (target.empty() || target[0] != '/' || target.find("..") != std::string::npos)
+      {
+         send_bad_response(
+            http::status::not_found,
+            "File not found\r\n");
+         return;
+      }
+
+      std::string full_path = doc_root_;
+      full_path.append(
+         target.data(),
+         target.size());
+
+      http::file_body::value_type file;
+      beast::error_code ec;
+      file.open(
+         full_path.c_str(),
+         beast::file_mode::read,
+         ec);
+      if (ec)
+      {
+         send_bad_response(
+            http::status::not_found,
+            "File not found\r\n");
+         return;
+      }
+
+      file_response_.emplace(
+         std::piecewise_construct,
+         std::make_tuple(),
+         std::make_tuple(alloc_));
+
+      file_response_->result(http::status::ok);
+      file_response_->keep_alive(false);
+      file_response_->set(http::field::server, "Beast");
+      file_response_->set(http::field::content_type, mime_type(std::string(target)));
+      file_response_->body() = std::move(file);
+      file_response_->prepare_payload();
+
+      http::async_write(
+         socket_,
+         *file_response_,
+         [this](beast::error_code ec, std::size_t)
+      {
+         socket_.shutdown(tcp::socket::shutdown_send, ec);
+         file_response_.reset();
+         accept();
+      });
+   }
+
+   void check_deadline()
+   {
+      // The deadline may have moved, so check it has really passed.
+      if (request_deadline_.expiry() <= std::chrono::steady_clock::now())
+      {
+         // Close socket to cancel any outstanding operation.
+         socket_.close();
+
+         // Sleep indefinitely until we're given a new deadline.
+         request_deadline_.expires_at(
+            (std::chrono::steady_clock::time_point::max)());
+      }
+
+      request_deadline_.async_wait(
+         [this](beast::error_code)
+      {
+         check_deadline();
+      });
+   }
+};
 
 namespace extreme_carpaccio_client {
 
