eb1d074979731fcf21dc3d712ef8434ce9d222dc
NADIF Antoine <Antoine.NADIF@murex.com>
2022-04-27 19:09:54 +0200

    Upgrade googletest version.

--- clients/cpp/cmake/FindGoogleTest.cmake
+++ clients/cpp/cmake/FindGoogleTest.cmake
@@ -3,7 +3,7 @@
 CPMAddPackage(
   NAME googletest
   GITHUB_REPOSITORY google/googletest
-  GIT_TAG release-1.10.0
-  VERSION 1.10.0
+  GIT_TAG release-1.11.0
+  VERSION 1.11.0
   OPTIONS "INSTALL_GTEST OFF" "gtest_force_shared_crt" "BUILD_GMOCK ON"
 )
