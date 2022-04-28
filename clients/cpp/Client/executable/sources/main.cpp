fe45d482b4e1300a1beaa4fee1775a83cd87dbdd
pdalesme <Patrice.DALESME@murex.com>
2022-04-28 15:49:44 +0200

    Add the possibility to pass ip and port as parameters of client executable

--- clients/cpp/Client/executable/sources/main.cpp
+++ clients/cpp/Client/executable/sources/main.cpp
@@ -5,10 +5,18 @@
 #include <cstdlib>
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-   std::cout << "Extreme Carpaccio server starting." << std::endl;
-   extreme_carpaccio::client::CarpaccioServer server(DEFAULT_HTTP_SERVER_IP, DEFAULT_HTTP_SERVER_PORT);
+   std::string ip = DEFAULT_HTTP_SERVER_IP;
+   unsigned short port = DEFAULT_HTTP_SERVER_PORT;
+
+   if (argc == 3)
+   {
+      ip = argv[1];
+      port = std::atoi(argv[2]);
+   }
+   std::cout << "Extreme Carpaccio server starting with ip " << ip << " and port " << port << "." << std::endl;
+   extreme_carpaccio::client::CarpaccioServer server(ip, port);
    server.start();
    std::cout << "Extreme Carpaccio server stopping." << std::endl;
 
